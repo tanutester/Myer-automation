a193f3be988ceb06a18ba01975199270e3442453
tanutester <tanupriya775@gmail.com>
2022-07-19 13:43:28 +1000

    created myer page objects

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,13 +1,7 @@
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
-module.exports = class Page {
-    /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
-    open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
-    }
+const openMyerPage = (path)=> {
+    return browser.url(`https://www.myer.com.au/${path}`)
+}
+
+module.exports = {
+    openMyerPage 
 }
--- test/specs/Myer.spec.js
+++ test/specs/Myer.spec.js
@@ -1,14 +1,13 @@
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
 
-describe('My Login application', () => {
+describe('Myer login page', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
+        await LoginPage.openloginPage();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+       /* await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+            'You logged into a secure area!');*/
     });
 });
 
